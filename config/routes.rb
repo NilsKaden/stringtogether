1f93a7d9356c5423456102830c31818217b838b5
Nils Kaden <nils.kaden94@gmail.com>
2018-02-13 21:06:47 +0000

    more routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#index'
   get '/hello/', to: 'application#hello'
+  get '/index/', to: 'application#index'
 end
